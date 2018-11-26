19e86f654974ccd7c96bfbb9c54d312f20857fea
Li <LIXJ3@legal.regn.net>
2018-11-26 14:28:35 +0800

    no message

---
+++ src/test/java/com/jl/dao/testsp.java
@@ -0,0 +1,12 @@
+package com.jl.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/applicationContext-dao.xml"})
+public class testsp {
+
+}

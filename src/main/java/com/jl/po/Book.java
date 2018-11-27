28d19847cc853c2d5e4a46e57364f214a97d92b4
Li <LIXJ3@legal.regn.net>
2018-11-27 09:06:57 +0800

    no message

---
+++ src/main/java/com/jl/po/Book.java
@@ -0,0 +1,53 @@
+package com.jl.po;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Book implements Serializable {
+	
+	private long ID;
+	private String Name;
+	private String Author;
+	private Integer Lenght;
+	private Integer Favor;
+	public long getID() {
+		return ID;
+	}
+	public void setID(long iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getAuthor() {
+		return Author;
+	}
+	public void setAuthor(String author) {
+		Author = author;
+	}
+	public Integer getLenght() {
+		return Lenght;
+	}
+	public void setLenght(Integer lenght) {
+		Lenght = lenght;
+	}
+	public Integer getFavor() {
+		return Favor;
+	}
+	public void setFavor(Integer favor) {
+		Favor = favor;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book [ID=" + ID + ", Name=" + Name + ", Author=" + Author + ", Lenght=" + Lenght + ", Favor=" + Favor
+				+ "]";
+	}
+	
+	
+}
--- src/test/java/com/jl/dao/testsp.java
+++ src/test/java/com/jl/dao/testsp.java
@@ -1,12 +1,29 @@
 package com.jl.dao;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.jl.po.Book;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/applicationContext-dao.xml"})
 public class testsp {
 
+	
+	
+	@Test
+	public void test1() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("spring/applicationContext-dao.xml");
+		Book book = ac.getBean("book", Book.class);
+		
+		book.setID(1);
+		
+		System.out.println(book.getID());
+		
+	}
 }
